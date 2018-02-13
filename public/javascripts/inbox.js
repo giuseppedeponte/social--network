0fe471d49d9be2f07cbd95f6657f35302301cfe1
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2018-02-13 08:25:24 +0100

    Inbox layout

---
+++ public/javascripts/inbox.js
@@ -0,0 +1,17 @@
+$(function() {
+  var $quote = $(".blockquote p");
+  for (var i = 0; $quote[i]; i += 1) {
+    var $numWords = $($quote[i]).text().split(" ").length;
+    if (($numWords >= 1) && ($numWords < 10)) {
+      $($quote[i]).css("font-size", "28px");
+    } else if (($numWords >= 10) && ($numWords < 20)) {
+      $($quote[i]).css("font-size", "24px");
+    } else if (($numWords >= 20) && ($numWords < 30)) {
+      $($quote[i]).css("font-size", "20px");
+    } else if (($numWords >= 30) && ($numWords < 40)) {
+      $($quote[i]).css("font-size", "16px");
+    } else {
+      $($quote[i]).css("font-size", "14px");
+    }
+  }
+});
--- routes/inbox.js
+++ routes/inbox.js
@@ -6,9 +6,52 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.get('/:userId', auth.owner, (req, res, next) => {
+  let user = Object.assign({}, req.user);
+  user.topics = [
+    {
+      date: new Date(),
+      subject: 'Topic Subject',
+      messages: [{
+        author: 'User',
+        text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
+        date: new Date()
+      },
+      {
+        author: 'User',
+        text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.",
+        date: new Date()
+      },
+      {
+        author: 'User',
+        text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+        date: new Date()
+      }],
+      subscribers: []
+    },
+    {
+      date: new Date(),
+      subject: 'Topic Subject',
+      messages: [{
+        author: 'User',
+        text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
+        date: new Date()
+      },
+      {
+        author: 'User',
+        text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.",
+        date: new Date()
+      },
+      {
+        author: 'User',
+        text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.",
+        date: new Date()
+      }],
+      subscribers: []
+    }
+  ];
   res.render('inbox', {
     title: 'Messagerie',
-    user: req.user,
+    viewer: user,
     isOwner: req.isOwner,
     isFriend: req.isFriend,
     isAdmin: req.isAdmin,
