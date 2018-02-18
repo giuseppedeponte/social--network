4db7e6653df466d21d2366a091e92ef249cd933e
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2018-02-18 18:44:39 +0100

    Add stats on index page

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,65 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const User = require('../models/User');
+const Topic = require('../models/Topic');
+const Post = require('../models/Post');
+const Conversation = require('../models/Conversation');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Social Network',
-    user: req.user || null,
-    viewer: req.user || null,
-    flashMessage: req.flash('flashMessage')
-  });
+  let stats = {};
+  Promise.all([
+    User
+    .count()
+    .then((number) => {
+      stats.users = number;
+    }),
+    User
+    .find({
+      'socket': {
+        $ne: ''
+      }
+    })
+    .count()
+    .then((number) => {
+      stats.connections = number;
+    }),
+    Topic
+    .count()
+    .then((number) => {
+      stats.topics = number;
+    }),
+    Post
+    .count()
+    .then((number) => {
+      stats.posts = number;
+    }),
+    Conversation
+    .count()
+    .then((number) => {
+      stats.chats = number;
+    })
+  ])
+  .then(() => {
+    console.log(stats);
+    res.render('index', {
+      title: 'Social Network',
+      user: req.user || null,
+      viewer: req.user || null,
+      stats: stats,
+      flashMessage: req.flash('flashMessage')
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.render('index', {
+      title: 'Social Network',
+      user: req.user || null,
+      viewer: req.user || null,
+      flashMessage: req.flash('flashMessage')
+    });
+  })
 });
 
 module.exports = router;
